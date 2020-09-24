b9691d7d34c23b499d90aa687fd8a1c697a7b8a8
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-09-25 02:08:49 +0530

    added whitelist and forbidNonWhitelisted in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // If true, It will remove the additional data from post request which is not in dto
+    forbidNonWhitelisted: false // if trye server will throw error with exact extra data in payload
+  }))
   await app.listen(3000);
 }
 bootstrap();
